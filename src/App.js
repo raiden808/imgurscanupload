cc43fe32ae8fe738cee846ee97d8579254f0624c
raiden808 <raiden.nalasa@gmail.com>
2019-11-18 22:07:38 +0000

    fix css

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h2>Hello</h2>
-      <p>This is a font color test</p>
+      
+      <div className="scanContainer">
+        <h2>Text Scanner</h2>
+        <p>Scans you image for text.</p>
+        <div className="upload-btn-wrapper">
+          <button className="btn">Upload a file</button>
+          <input type="file" name="myfile" />
+        </div>
+      </div>
     </div>
   );
 }
