1e9bd5399aeae7728447f37ff69ceb39af7b7e7c
raiden808 <raiden.nalasa@gmail.com>
2019-11-18 22:22:46 +0000

    UI update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      
-      <div className="scanContainer">
-        <h2>Text Scanner</h2>
-        <p>Scans you image for text.</p>
-        <div className="upload-btn-wrapper">
-          <button className="btn">Upload a file</button>
-          <input type="file" name="myfile" />
+      <div className="container">
+        <div className="scanContainer">
+          <h2>Text Scanner</h2>
+          <p>Scans your image for text.</p>
+          <div className="upload-btn-wrapper">
+            <button className="btn">Upload a file</button>
+            <input type="file" name="myfile" />
+          </div>
         </div>
       </div>
     </div>
