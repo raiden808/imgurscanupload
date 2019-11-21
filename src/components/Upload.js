703ef4a6d0ec7ad2ce58e8ab4918809bcefc2ee5
raiden808 <raiden.nalasa@gmail.com>
2019-11-21 05:30:31 +0000

    file upload axios

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,14 +1,59 @@
-import React from 'react'
+import React,{ useState} from 'react'
+import axios from 'axios'
 
 const Upload = () =>{
     
+    const [toUpload,setUpload] = useState();
+    
     const onChangeHandler = (e) =>{
         // file object data
-        console.log(e.target.files[0])
+        
+        console.log(e.target.files)
+        
+        setUpload(e.target.files);
+        
+        console.log("axios triggered")
+        
+       
+        
+        var apiUrl = 'https://api.imgur.com/3/image';
+        var apiKey = '41491dab8a7c338';
+        
+        const data = new FormData()
+        data.append('image',e.target.files[0])
+            
+        axios.post(apiUrl, data, {
+            headers: {
+                'Authorization': 'Client-ID ' + apiKey,
+                'content-type': 'multipart/form-data'
+            }
+        }).then(res => { // then print response status
+	            console.log(res)
+            }
+        )
     }
     
-    const processUpload = (e) =>{
+    
+    //todo - set up api upload for imgur
+    const processUpload = (fileObject) =>{
         
+        console.log("File Data: ",fileObject)
+        
+        // const data = new FormData()
+        // var apiUrl = 'https://api.imgur.com/3/image';
+        // var apiKey = '41491dab8a7c338';
+        
+        // data.append('file',fileObject)
+            
+        // axios.post(apiUrl, data, {
+        //     headers: {
+        //         'authorization': 'Client-ID ' + apiKey,
+        //         'content-type': 'multipart/form-data'
+        //     }
+        // }).then(res => { // then print response status
+	       //     console.log(res)
+        //     }
+        // )
     } 
     
     return (
