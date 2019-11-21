42736c59bda4f123d30e68adaf10d2d85e77bab0
raiden808 <raiden.nalasa@gmail.com>
2019-11-21 02:47:28 +0000

    process file upload

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Upload = () =>{
     
+    const onChangeHandler = (e) =>{
+        // file object data
+        console.log(e.target.files[0])
+    }
     
+    const processUpload = (e) =>{
+        
+    } 
     
     return (
         <div className="scanContainer">
@@ -10,7 +17,11 @@ const Upload = () =>{
             <p>Scans your image for text.</p>
             <div className="upload-btn-wrapper">
                 <button className="btn">Upload a file</button>
-                <input type="file" name="myfile" />
+                <input 
+                    type="file" 
+                    name="myfile" 
+                    onChange={(e) =>onChangeHandler(e)}
+                />
             </div>    
         </div>
     )
